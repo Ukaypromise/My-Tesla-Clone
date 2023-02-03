4db8217f8c0e96ad9e8438a39d159ab16830dcaf
Promise Uka <promiseukaimo@gmail.com>
2023-02-03 16:32:45 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
-import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
-//       <Main />
+
       <Header />
       <Home />
     </div>
